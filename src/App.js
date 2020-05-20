e8aa185d46cf99d492dc25d148794e4466d0b115
Sacha Sedriks <sachased@gmail.com>
2020-05-20 16:35:03 -0700

    npx create-react-app boilerplate.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// per: https://courses.thinkful.com/react-v1/checkpoint/12
+// reference repo: https://github.com/Thinkful-Ed/react-fileuploader
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      
+    </main>
   );
 }
-
-export default App;
